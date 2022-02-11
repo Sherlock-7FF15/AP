b367ecc71455759c1199556d955bf3712dce3e4c
sherlock <1091664180@qq.com>
2022-02-11 17:19:24 +0800

    2/11

--- untitled/src/sovle.java
+++ untitled/src/sovle.java
@@ -58,6 +58,20 @@ public class sovle {
         return min;
     }
 
+    public void al() {
+        String[] strs = {"abcd", "kbcd", "cbcd"};
+        String tem = "";
+        if (strs == null || strs.length == 0) tem = "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    tem = strs[0].substring(0, i);
+            }
+        }
+        System.out.println(tem);
+    }
+
 
     static Comparator<Integer> comparator = new Comparator<Integer>() {
         @Override
